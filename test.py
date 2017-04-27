31416f0776dbf19255715d4c3f2c243dcd90d163
Ying Xu <xuyying@gmail.com>
2017-04-27 09:17:23 -0700

    change 2

--- test.py
+++ test.py
@@ -1,4 +1,13 @@
+# Mesaure some strings
+words = ['Monday', 'Tuesday', 'Wednesday']
+for w in words:
+    print (w, len(w))
+
+# Fibonacci series
 a, b = 0, 1
+if b > 0:
+    print ('Non zero')
+
 while b < 100:
     print (b, end=',')
     a, b = b, a+b
